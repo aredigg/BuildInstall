3c91003ab8864224d90e7a8793085a339f4ce677
Are Digranes <ared@runbox.no>
2026-06-05 15:25:39 +0200

    setup

--- bi
+++ bi
@@ -12,16 +12,20 @@ zmodload zsh/zutil
 
 BI_VERSION="2.00"
 BI_SCRIPT="${ZSH_SCRIPT}"
+BI_SCRIPT_PID="$$"
 BI_STARTTIME="${EPOCHSECONDS}"
 BI_DIRECTORY="${0:A:h}"
 BI_SYSTEM_PREFIX="/usr/local"
+BI_SYSTEM_TMP="/tmp"
 
 source "$BI_DIRECTORY/src/setup.zsh"
 source "$BI_DIRECTORY/src/utility.zsh"
 
-print "$BI_SCRIPT $BI_VERSION $BI_STARTTIME"
 parse "$@"
-print "$INSTALL_COMMAND $INSTALL_PREFIX"
+setup
+
+
+print "$INSTALL_COMMAND $INSTALL_PREFIX $CONCURRENT_JOBS"
 if [[ -n "$INSTALL_UTILITY" ]]; then
     print "$INSTALL_UTILITY"
 fi
--- src/setup.zsh
+++ src/setup.zsh
@@ -1,7 +1,7 @@
 # Setup routines
 
 usage() {
-    printf "\
+    print -u 2 -f "\
 Usage: $BI_SCRIPT command [options]
 BuildInstall is an install script for building and installing various utilities
 Commands
@@ -15,20 +15,23 @@ Options
     -d, --debug     Turn on debug output
     -p, --prefix    Custom install prefix (Default $BI_SYSTEM_PREFIX)
     -u, --utility   Install, remove or print only the selected utility
+    -t, --temp      Temporary directory for downloads and build artifacts (Default $BI_SYSTEM_TMP)
+    -m, --manifest  Directory where manifest files are stored (Default $BI_SYSTEM_TMP/bi/manifests)
     -a, --archive   Create tar.xz archives of the built install
     -n, --nocolor   Plain output
 $1"
     if [[ -n $1 ]]; then
         exit 1
-    else
-        exit 0
     fi
+    exit 0
 }
 
 parse() {
     local valid_commands=("install" "download" "print" "remove" "help" "version")
     declare -A opts
     opts[-p]="$BI_SYSTEM_PREFIX"
+    opts[-t]="$BI_SYSTEM_TMP"
+    opts[-m]="$BI_SYSTEM_TMP/bi/manifests"
     zparseopts -A opts -D -E -F -K \
         h -help=h \
         v -verbose=v \
@@ -36,43 +39,108 @@ parse() {
         p: -prefix:=p \
         a -archive=a \
         u: -utility:=u \
+        t: -temp:=t \
+        m: -manifest:=m \
         n -nocolor=n \
         2>/dev/null || usage "ERROR: Invalid option entered or missing argument\n"
     print "command = <$1>"
     print "options = <${(k)opts}>"
     print "remaining = <$@>"
+
+    # Set the install prefix
     INSTALL_PREFIX="${opts[-p]#=}"
+
+    # Display usage
     if [[ -v opts[-h] || $1 = "help" ]]; then
         usage
     fi
+
+    # Check that we have a install command
     if [[ $# -eq 1 ]]; then
         INSTALL_COMMAND="$1"
     else
         usage "ERROR: Invalid or missing command\n"
     fi
+
+    # Display version
     if [[ $INSTALL_COMMAND = "version" ]]; then
         print "$BI_VERSION"
         exit 0
     fi
+
+    # Turn on debug if requested
     if [[ -v opts[-d] ]]; then
         DEBUG=ON
     else
         DEBUG=OFF
     fi
+
+    # Different temp directory
+    INSTALL_TEMP="${opts[-t]#=}"
+
+    # Different manifest directory
+    INSTALL_MANIFESTS="${opts[-m]#=}"
+
+    # Turn on verbose mode
     if [[ -v opts[-v] ]]; then
         VERBOSE=ON
     else
         VERBOSE=OFF
     fi
+
+    # If requested to not output "fancy" colors
     if [[ -v opts[-n] ]]; then
         PLAIN_OUTPUT=ON
     else
         PLAIN_OUTPUT=OFF
     fi
+
+    # If single utility is requested
     if [[ -v opts[-u] ]]; then
         INSTALL_UTILITY="${opts[-u]#=}"
     fi
 }
 
 setup() {
+    # Assert that we have an install command
+    if [[ ! -n INSTALL_COMMAND ]]; then
+        print -u 2 "ERROR: Missing install command"
+        exit 2
+    fi
+    # Custom settings for specific systems
+    INSTALL_SYSTEM=$(uname)
+    case "$INSTALL_SYSTEM" in
+        Darwin)
+            MACOS_SDK_PATH="$(xcrun --show-sdk-path)"
+            export MACOSX_DEPLOYMENT_TARGET="$(xcrun --show-sdk-platform-version)"
+            local cpus="$(getconf _NPROCESSORS_ONLN)"
+            CONCURRENT_JOBS=$(( $cpus + $cpus >> 1))
+            ;;
+        *) 
+            print -u 2 "ERROR: Not on a supported system" 
+            ;;
+    esac
+    # Setup directories
+    BUILD_SOURCES_DIRECTORY="${INSTALL_TEMP}/bi/sources"
+    BUILD_BUILDS_DIRECTORY="${INSTALL_TEMP}/bi/builds"
+    BUILD_LOGS_DIRECTORY="${INSTALL_TEMP}/bi/logs"
+    INSTALL_ARCHIVES="${INSTALL_TEMP}/bi/archives"
+    mkdir -p \
+        "$BUILD_SOURCES_DIRECTORY" \
+        "$BUILD_BUILDS_DIRECTORY" \
+        "$BUILD_LOGS_DIRECTORY" \
+        "$INSTALL_ARCHIVES" \
+        "$INSTALL_MANIFESTS"
+    BUILD_LOG_OUT="${BUILD_LOGS_DIRECTORY}/out.log"
+    BUILD_LOG_ERR="${BUILD_LOGS_DIRECTORY}/err.log"
+    # Redirect standard out and error
+    exec >"$BUILD_LOG_OUT" 2>"$BUILD_LOG_ERR"
+    # Keep sudo alive
+    { while kill -O "$BI_SCRIPT_PID" 2>/dev/null; do sudo -nv || exit; sleep 60 || exit; done 2>/dev/null & }
+    BI_SUDO_PID=$!
+    # Print script starttime into the logs
+    TZ=UTC strftime -s timefmt '%Y-%m-%d %H:%M:%S' "$BI_STARTTIME"
+    print -ru1 $timefmt
+    print -ru2 $timefmt
 }
+
